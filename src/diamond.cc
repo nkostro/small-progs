c255cc75a252e98e1696b8085d7f1a78afd0adda
Nikolai Kostromitin <emailbin@yandex.ru>
2019-12-12 08:56:55 +0500

    Refactoring
    Refactored program's logic

--- src/diamond.cc
+++ src/diamond.cc
@@ -1,58 +1,45 @@
-//  +--*--+
-//  | *|* |
-//  |* | *|
-//  *--+--*
-//  |* | *|
-//  | *|* |
-//  +--*--+
-
 #include <iostream>
 
-
-void paint_diamond(int side);
+void draw_diamond(unsigned side);
 
 
 int main()
 {
-    paint_diamond(11);
+    unsigned side;
+    std::cin >> side;
+    if (side % 2 == 0)
+        ++side;
+
+    draw_diamond(side);
 }
 
+// side == 9
+//
+//     *
+//    * *
+//   *   *
+//  *     *
+// *       *
+//  *     *
+//   *   *
+//    * *
+//     *
 
-void paint_diamond(int side)
+void draw_diamond(unsigned side)
 {
-    const int middle = side / 2;
+    const unsigned middle = side / 2;
     const char star = '*';
     const char blank = ' ';
 
-    for (int i = 0; i < middle; ++i) {
-        for (int j = middle; j >= 0; --j)
-            if (i == j)
+    for (unsigned i = 0; i < side; ++i) {
+        for (unsigned j = 0; j < side; ++j) {
+            if (i + j == middle || i + j == side - 1 + middle)
+                std::cout << star;
+            else if (i + middle == j || i == j + middle)
                 std::cout << star;
             else
                 std::cout << blank;
-
-        for (int j = middle+1; j < side; ++j)
-            if (i + middle == j)
-                std::cout << star;
-            else
-                std::cout << blank;
-
-        std::cout << std::endl;
-    }
-
-    for (int i = 0; i < side; ++i) {
-        for (int j = 0; j < middle+1; ++j)
-            if (i == j)
-                std::cout << star;
-            else
-                std::cout << blank;
-
-        for (int j = side-1; j > middle; --j)
-            if (i + middle + 1 == j)
-                std::cout << star;
-            else
-                std::cout << blank;
-
+        }
         std::cout << std::endl;
     }
 }
