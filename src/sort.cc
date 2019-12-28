02d92bb7bfeb924eadfc448373b0627d1a4d4d44
Nikolai Kostromitin <emailbin@yandex.ru>
2019-12-28 22:05:42 +0500

    Small enhancement.

--- src/sort.cc
+++ src/sort.cc
@@ -10,7 +10,7 @@
 #include <ctime>
 
 void fill(int ar[], int size, int a, int b);
-void sort(int ar[], int size, bool is_decreasing);
+void sort(int ar[], int size, bool desc = false);
 void print(const int a[], int size, int width);
 
 int main()
@@ -18,19 +18,19 @@ int main()
     const int size = 10;
     int a[size];
 
-    fill(a, size, -100, 201);
+    fill(a, size, -100, 100);
 
     const int width = 5;
     std::cout << "Before sort: ";
     print(a, size, width);
     std::cout << std::endl;
 
-    bool is_decreasing = false;
-    if (is_decreasing)
+    bool desc = false;
+    if (desc)
         std::cout << "... sorting in decreasing order\n";
     else
         std::cout << "... sorting in increasing order\n";
-    sort(a, size, is_decreasing);
+    sort(a, size, desc);
 
     std::cout << "After sort: ";
     print(a, size, width);
@@ -41,7 +41,7 @@ void fill(int ar[], int size, int a, int b)
 {
     srand(time(NULL));
     for (int i = 0; i < size; ++i)
-        ar[i] = rand() % b + a;
+        ar[i] = rand() % (b - a + 1) + a;
 }
 
 void swap(int *a, int *b) { int t = *a; *a = *b; *b = t; }
@@ -66,9 +66,9 @@ bool cmp_lt(int a, int b) { return a < b; }
 
 bool cmp_gt(int a, int b) { return a > b; }
 
-void sort(int a[], int size, bool is_decreasing)
+void sort(int a[], int size, bool desc)
 {
-    if (is_decreasing)
+    if (desc)
         sort(a, size, [] (int a, int b) { return a < b; });
     else
         sort(a, size, [] (int a, int b) { return a > b; });
